9835d50b575454faf130c76df140ed7601d37279
tarikdincer <51008424+tarikdincer@users.noreply.github.com>
2021-11-29 04:46:53 +0300

    final commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import template
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 import io
 import os
 import argparse
@@ -88,20 +88,17 @@ def predict_entities(resume_text, researcherid):
 def get_pubs(rid):
     # Retrieve the author's data, fill-in, and print
     res = selectResearcher(researcherid=rid)[0]
-    print(res)
     rname = res[1] + " " + res[2]
-    search_query = scholarly.search_keyword(rname)
-    print("query", search_query)
-    author = scholarly.fill(next(search_query))
-    print(author)
+    search_query = scholarly.search_pubs(rname)
+    publication = next(search_query, None)
+    counter = 0
 
-    # Print the titles of the author's publications
-    print([pub['bib']['title'] for pub in author['publications']])
-
-    # Take a closer look at the first publication
-    pub = scholarly.fill(author['publications'][0])
-    print(pub)
-        
+    while publication is not None and counter < 10:
+        publication = next(search_query, None)
+        #print(publication)
+        pid = insertPublication(ptitle = publication["bib"]["title"], pyear = publication["bib"]["pub_year"], venue = publication["bib"]["venue"], scholarurl = publication["pub_url"], bibtex = scholarly.bibtex(publication))
+        insertCoauthor(pid, rid)
+        counter += 1    
 
 
 @app.route("/cv_sent", methods=['POST', 'GET'])
@@ -111,13 +108,11 @@ def cv_sent():
         name = result["Name"]
         surname = result["Surname"]
         website_url = result["Url"]
-        print(name, surname)
         rid = insertResearcher(name, surname)
-        #get_pubs(rid)
+        get_pubs(rid)
 
         if website_url:
             try:
-                print(website_url)
                 web_content = parse_html(website_url)
                 predict_entities(resume_text=web_content, researcherid = rid)
             except:
@@ -137,7 +132,6 @@ def cv_sent():
             elif '.doc' in filename:
                 try:
                     content = condense_newline(textract.process(filename).decode('utf-8'))
-                    print("content", content)
                     predict_entities(resume_text=content)
                 except:
                     print(filename, "doc not found")
@@ -149,8 +143,8 @@ def cv_sent():
                     print("pdf not found")
             # f.save(os.path.join(app.config['UPLOAD_FOLDER'],\
             #          filename))
-            print(filename)
-        return render_template("index.html")
+            #doc_pdf = weasyprint.HTML(url_for('cv_create', researcherid = rid)).write_pdf(name + '_' + surname + '_cv.pdf')
+            return redirect(url_for('cv_create', researcherid = rid))
 
 @app.route("/")
 def index():
@@ -159,17 +153,53 @@ def index():
 @app.route("/cv_create", methods=['POST', 'GET'])
 def cv_create():
     rid = request.args.get('researcherid')
+    if rid is None:
+        return "Please add researcherid parameter"
     researcher = selectResearcher(researcherid=rid)
+    educations = selectEducation(researcherid=rid)
+    works = selectWork(researcherid=rid)
     skills = selectSkill(researcherid=rid)
-    skill_html = ""
+    pubs = selectPublication(researcherid=rid)
+    education_html = "<h3> Education </h3>"
+    for education in educations:
+        education_html += f'''<div>
+        <h5> {education[0]} </h5>
+        <p> {education[1]} </p>
+        </div>'''
+    work_html = "<h3> Work Experience </h3>"
+    for work in works:
+        work_html += f'''<div>
+        <h5> {work[0]} </h5>
+        <p> {work[1]} </p>
+        </div>'''
+    skill_html = "<h3> Skills </h3>"
     for skill in skills:
         skill_html += f'''<div>
         <p> {skill[1]} </p>
         </div>'''
-    return f"""<div>
-    <h3>{researcher[0][1]} {researcher[0][2]}</h3>
-    {skill_html}
-    </div>"""
+    pub_html = "<h3> Publications </h3>"
+    for pub in pubs:
+        pub_html += f'''<div>
+        <h5> {pub[0]} ({pub[1]}) </h5>
+        </div>'''
+
+    return f"""
+    <!DOCTYPE html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+        <title>Cv Generator</title>
+    </head>
+    <body>
+        <div class="container-fluid p-5 bg-primary text-white text-center">
+            <h2>{researcher[0][1]} {researcher[0][2]}</h2>
+            {education_html}
+            {work_html}
+            {skill_html}
+            {pub_html}
+        </div>
+    </body>
+    """
 
 def insertResearcher(rname = "", rlastname = "", orchid=0):
     cur.execute(f"INSERT INTO researcher (rname, rlastname, orchid) \
@@ -209,14 +239,41 @@ def insertEducation(researcherid = 0, organizationid = 0, edegree = "", edepartm
 def selectResearcher(researcherid):
     cur.execute(f"SELECT * FROM researcher WHERE researcherid = {researcherid};")
     rows = cur.fetchall()
-    print("asdasdasd ", rows)
     return rows
 
 def selectSkill(researcherid):
     cur.execute(f"SELECT * FROM skill WHERE researcherid = {researcherid};")
     rows = cur.fetchall()
-    print("asdasdasd ", rows)
+    return rows
+
+def insertPublication(ptitle = "", pyear = 0, ptype = "", venue = "", doi = 0, scholarurl = "", bibtex = ""):
+    cur.execute(f"INSERT INTO publication (ptitle, pyear, ptype, venue, doi, scholarurl, bibtex) \
+      VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING publicationid;", (ptitle, pyear, ptype, venue, doi, scholarurl, bibtex))
+    rows = cur.fetchall()
+    conn.commit()
+    return rows[0][0]
+
+def insertCoauthor(publicationid = 0, researcherid = 0):
+    cur.execute(f"INSERT INTO co_author (publicationid, researcherid) \
+      VALUES ({publicationid}, {researcherid}) RETURNING publicationid;")
+    rows = cur.fetchall()
+    conn.commit()
+    return rows[0][0]
+
+def selectPublication(researcherid):
+    cur.execute(f"SELECT ptitle, pyear, venue FROM co_author, publication WHERE co_author.publicationid = publication.publicationid AND co_author.researcherid = {researcherid};")
+    rows = cur.fetchall()
+    return rows
+
+def selectEducation(researcherid):
+    cur.execute(f"SELECT edegree,oname FROM education, organization WHERE education.organizationid = organization.organizationid AND education.researcherid = {researcherid};")
+    rows = cur.fetchall()
+    return rows
+
+def selectWork(researcherid):
+    cur.execute(f"SELECT wtitle,oname FROM work, organization WHERE work.organizationid = organization.organizationid AND work.researcherid = {researcherid};")
+    rows = cur.fetchall()
     return rows
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ create_tables.sql
@@ -0,0 +1,140 @@
+CREATE TABLE IF NOT EXISTS RESEARCHER (
+   ResearcherID SERIAL UNIQUE NOT NULL,
+   RName VARCHAR ( 50 ) NOT NULL,
+	RLastName VARCHAR ( 50 ) NOT NULL,
+	ORCHID integer,
+	PRIMARY KEY (ResearcherID)
+);
+CREATE TABLE IF NOT EXISTS SERVICE (
+	ServiceID SERIAL UNIQUE NOT NULL,
+    SWhere VARCHAR ( 50 ),
+	SRole VARCHAR ( 50 ),
+	SYear Date,
+	ResearcherID integer NOT NULL,
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, ServiceID)
+);
+CREATE TABLE IF NOT EXISTS AWARD (
+    AwardID SERIAL UNIQUE NOT NULL,
+    AName VARCHAR ( 50 ) NOT NULL,
+	AYear Date,
+	ResearcherID integer NOT NULL,
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, AwardID)
+);
+CREATE TABLE IF NOT EXISTS GIVEN_COURSE (
+    CourseID SERIAL UNIQUE NOT NULL,
+    CName VARCHAR ( 50 ) NOT NULL,
+	Code integer,
+	CYear Date,
+	CSemester VARCHAR ( 20 ) ,
+    ResearcherID integer NOT NULL,
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, CourseID)
+);
+CREATE TABLE IF NOT EXISTS SKILL (
+    SkillID SERIAL UNIQUE NOT NULL,
+    SName VARCHAR ( 50 ) NOT NULL,
+	ProficiencyLevel integer,
+	ResearcherID integer NOT NULL, 
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, SkillID)
+);
+
+CREATE TABLE IF NOT EXISTS PUBLICATION (
+	PublicationID SERIAL UNIQUE NOT NULL ,
+    PTitle VARCHAR ( 500 ) NOT NULL,
+	PYear VARCHAR( 20 ),
+    PType VARCHAR ( 50 ),
+	Venue VARCHAR ( 500 ),
+	DOI integer UNIQUE,
+	ScholarURL VARCHAR ( 520 ),
+	BibTex VARCHAR ( 2500 ),
+	PRIMARY KEY (PublicationID)
+
+);
+CREATE TABLE IF NOT EXISTS SUPERVISED_THESIS (
+    SupervisedThesisID SERIAL UNIQUE NOT NULL,
+    STName VARCHAR ( 50 ),
+	STLastName VARCHAR ( 50 ),
+	STDegree VARCHAR ( 20 ),
+	Code integer,
+	CYear Date,
+	CSemester VARCHAR ( 20 ),
+	ResearcherID integer NOT NULL, 
+	PublicationID integer NOT NULL, 
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	FOREIGN KEY (PublicationID)
+    REFERENCES PUBLICATION (PublicationID),
+	PRIMARY KEY (ResearcherID, SupervisedThesisID)
+
+);
+CREATE TABLE IF NOT EXISTS CO_AUTHOR (
+	PublicationID integer  NOT NULL ,
+	ResearcherID integer  NOT NULL, 
+	FOREIGN KEY (PublicationID)
+    REFERENCES PUBLICATION (PublicationID),
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, PublicationID)
+
+);
+CREATE TABLE IF NOT EXISTS PHRASE (
+	PhraseID SERIAL UNIQUE NOT NULL ,
+	PName VARCHAR ( 520 ) NOT NULL,
+	PRIMARY KEY (PhraseID)
+
+);
+CREATE TABLE IF NOT EXISTS INTEREST (
+	ResearcherID integer  NOT NULL ,
+	PhraseID integer  NOT NULL, 
+	FOREIGN KEY (PhraseID)
+    REFERENCES PHRASE (PhraseID),
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, PhraseID)
+
+);
+CREATE TABLE IF NOT EXISTS ORGANIZATION (
+    OrganizationID SERIAL UNIQUE NOT NULL,
+    OName VARCHAR ( 50 ) NOT NULL,
+	OCity VARCHAR ( 50 ),
+	OState VARCHAR ( 50 ),
+	OCountry VARCHAR ( 50 ),
+	PRIMARY KEY (OrganizationID)
+
+);
+CREATE TABLE IF NOT EXISTS WORK (
+	ResearcherID integer  NOT NULL ,
+	OrganizationID integer  NOT NULL, 
+	WTitle VARCHAR ( 50 ),
+	WDepartment VARCHAR ( 50 ),
+	StartYear Date,
+	EndYear Date,
+	FOREIGN KEY (OrganizationID)
+    REFERENCES ORGANIZATION (OrganizationID),
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, OrganizationID)
+
+);
+
+CREATE TABLE IF NOT EXISTS EDUCATION (
+	ResearcherID integer  NOT NULL ,
+	OrganizationID integer  NOT NULL, 
+	EDegree VARCHAR ( 50 ),
+	EDepartment VARCHAR ( 50 ),
+	StartYear Date,
+	EndYear Date,
+	FOREIGN KEY (OrganizationID)
+    REFERENCES ORGANIZATION (OrganizationID),
+	FOREIGN KEY (ResearcherID)
+    REFERENCES RESEARCHER (ResearcherID),
+	PRIMARY KEY (ResearcherID, OrganizationID)
+
+);
